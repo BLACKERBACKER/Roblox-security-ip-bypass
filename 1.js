e3b58af083e177a97820dcab139bf6d03e3041e8
BLACKERBACKER <162949789+BLACKERBACKER@users.noreply.github.com>
2024-03-18 01:00:30 -0400

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IP Bypass</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="container">
+        <input type="text" id="cookieInput" placeholder="Cookie Goes Here..." onfocus="clearText()">
+        <button onclick="sendMessage()">IP Bypass</button>
+    </div>
+
+    <script src="script.js"></script>
+</body>
+</html>
